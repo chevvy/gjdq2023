4de7e950d8a104c109fa86973c51e61790aea3a8
Vincent Chavanel Jobin <vincentcjobin@gmail.com>
2023-09-23 15:03:38 -0400

    fix local spawning

--- Assets/ModelManager.cs
+++ Assets/ModelManager.cs
@@ -28,7 +28,7 @@ public class ModelManager : MonoBehaviour
         defaultModel.SetActive(false);
         Vector3 position = gameObject.transform.position;
         // Vector3 newVec = new Vector3(position.x, 5f, position.z);
-        currentModel = Instantiate(model, transform);
+        currentModel = Instantiate(model, transform, true);
         if (currentModel.TryGetComponent(out Rigidbody rb))
         {
             rb.isKinematic = true;
