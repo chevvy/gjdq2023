fe2a19e0c2462fb85e2fae2aefd6c34312eefcf7
alix <lelievre.alix@gmail.com>
2023-09-23 22:47:32 -0400

    Game (lvl1) end is now a coroutine that ends the game after x seconds

--- Assets/GameStateHandler/GameOverHandler.cs
+++ Assets/GameStateHandler/GameOverHandler.cs
@@ -1,8 +1,12 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverHandler : MonoBehaviour
 {
+    private bool _isEnding = false;
+    private float _endGameDelay = 2.0f;
+
     public void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag(Tags.Runner))
@@ -12,6 +16,16 @@ public class GameOverHandler : MonoBehaviour
     public void HandlePlayerReachedObjective(GameObject runner)
     {
         runner.GetComponent<RunnerController>().StartIdleAnimation();
+
+        if (!_isEnding)
+            StartCoroutine(BeginEndGameTransition(_endGameDelay));
+    }
+
+    IEnumerator BeginEndGameTransition(float t)
+    {
+        _isEnding = true;
+
+        yield return new WaitForSeconds(t);
 
         SceneManager.LoadScene(SceneIndex.GameScene);
     }
