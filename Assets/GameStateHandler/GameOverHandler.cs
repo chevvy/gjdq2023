049c0dbf271ab2f0608f7dad556807f737162a37
alix <lelievre.alix@gmail.com>
2023-09-23 22:34:15 -0400

    Dynamically set runners according to the playing players that entered in l0.

--- Assets/GameStateHandler/GameOverHandler.cs
+++ Assets/GameStateHandler/GameOverHandler.cs
@@ -6,17 +6,13 @@ public class GameOverHandler : MonoBehaviour
     public void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag(Tags.Runner))
-        {
             HandlePlayerReachedObjective(collider.gameObject);
-        }
     }
 
     public void HandlePlayerReachedObjective(GameObject runner)
     {
-        Debug.Log($"{runner.name} wins");
-
         runner.GetComponent<RunnerController>().StartIdleAnimation();
 
-        SceneManager.LoadScene(SceneIndex.EndGameScene);
+        SceneManager.LoadScene(SceneIndex.GameScene);
     }
 }
--- Assets/SceneIndex.cs
+++ Assets/SceneIndex.cs
@@ -2,5 +2,4 @@ public static class SceneIndex
 {
     public static int MenuScene = 0;
     public static int GameScene = 1;
-    public static int EndGameScene = 2;
 }
